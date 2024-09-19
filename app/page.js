e351ced7cfdd01984f05258300d0a49c3b642be7
mariam maher <mahermariam367@gmail.com>
2024-09-20 02:29:11 +0300

    first commit with the first and basic version

---
+++ app/page.js
@@ -0,0 +1,17 @@
+import Category from "./components/home/category";
+import Projects from "./components/home/projects";
+import Review from "./components/home/review";
+import Header from "./components/home/header";
+import FAQ from "./components/home/FAQ";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Category />
+      <Projects />
+      <Review />
+      <FAQ />
+    </>
+  );
+}
